91a37af8631438d0a4a4f011e80ba509d1651be9
epicrunze <runze.zhang2001@gmail.com>
2020-05-27 16:01:42 -0400

    debugging

--- unirep.py
+++ unirep.py
@@ -438,6 +438,7 @@ class babbler1900():
                 # all hidden states
             int_seq_list = np.array(int_seq_list)
             print(int_seq_list.shape)
+            print(int_seq_list)
             final_state_, hs = sess.run(
                 [self._final_state, self._output], feed_dict={
                     self._batch_size_placeholder: num_seqs,
